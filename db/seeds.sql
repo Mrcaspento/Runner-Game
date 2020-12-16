1230f8cd6b74d0ec3b603b45eefed2a7c9ff6e71
Paul Carr <pwcarr15@gmail.com>
2020-12-16 14:11:23 -0500

    Added starter profiles for the creators in the seeds.sql.

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (carrpw, abc123, 15);
+
+INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (memeMaster, squidward, 1);
+
+INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (lucas, asdf, 1);
+
+INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (randy, qwerty, 1);
--- models/user.js
+++ models/user.js
@@ -18,6 +18,9 @@ module.exports = function(sequelize, DataTypes) {
         len: [1, 15]
       }
     },
+    score: {
+      type: DataTypes.INTEGER
+    },
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
