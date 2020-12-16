5840c881cd21ad49972cf6808837d299893bb5dc
Paul Carr <pwcarr15@gmail.com>
2020-12-16 14:27:24 -0500

    Minor changes to the seeds.sql.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (carrpw, abc123, 15);
 
-INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (memeMaster, squidward, 1);
+INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (memeMaster, qwerty, 1);
 
-INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (lucas, asdf, 1);
+INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (lucas, lucaslu, 1);
 
-INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (randy, qwerty, 1);
+INSTERT INTO gingerbreadMan_db (username, password, score) VALUES (randy, randy, 1);
